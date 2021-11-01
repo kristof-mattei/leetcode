a37fa307247860f4071f31a569506b1bc8544045
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2021-10-31 23:16:23 -0700

    feat: grcov -> tarpaulin

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -4,3 +4,10 @@ fn assert_world_ok() {
     let cls2 = || true;
     assert_eq!(cls1(), cls2());
 }
+
+#[test]
+fn assert_world_ok2() {
+    let cls1 = || false;
+    let cls2 = || false;
+    assert_eq!(cls1(), cls2());
+}
