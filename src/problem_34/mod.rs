e63555d2a3320d99b93b48e501fdff2d361ef6c1
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-02-26 20:48:16 -0800

    chore: problem 34 wip

---
+++ src/problem_34/mod.rs
@@ -0,0 +1,35 @@
+use crate::shared::Solution;
+
+impl Solution {
+    // #[must_use]
+    // #[allow(clippy::needless_pass_by_value)]
+    // pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+    //     search(&nums, target)
+    // }
+}
+
+#[cfg(test)]
+mod test {
+    // use crate::problem_33::search;
+
+    // #[test]
+    // fn test() {
+    //     let input = vec![4, 5, 6, 7, 0, 1, 2];
+
+    //     assert_eq!(search(&input, 0), 4);
+    // }
+
+    // #[test]
+    // fn test_2() {
+    //     let input = vec![1];
+
+    //     assert_eq!(search(&input, 0), -1);
+    // }
+
+    // #[test]
+    // fn test_3() {
+    //     let input = vec![5, 6, 7, 8, 1, 2, 3, 4];
+
+    //     assert_eq!(search(&input, 2), 5);
+    // }
+}
