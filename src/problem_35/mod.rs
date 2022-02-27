aef15ae9f1d0f20071d369825715ec934cbdd950
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-02-26 21:10:53 -0800

    chore: problem 35 wip

---
+++ src/problem_35/mod.rs
@@ -0,0 +1,12 @@
+use crate::shared::Solution;
+
+impl Solution {
+    // #[must_use]
+    // #[allow(clippy::needless_pass_by_value)]
+    // pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    //     search_range(&nums, target)
+    // }
+}
+
+#[cfg(test)]
+mod test {}
