93a88b809f0aec5651c905692e126412ae02e3b0
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-03-09 08:19:58 -0800

    feat: problem 72

---
+++ src/problem_70/mod.rs
@@ -0,0 +1,46 @@
+use std::collections::HashMap;
+
+use crate::shared::Solution;
+
+fn climb_stairs_r(cache: &mut HashMap<i32, i32>, n: i32) -> i32 {
+    if cache.contains_key(&n) {
+        return cache[&n];
+    }
+
+    let result = match n {
+        0 => 0,
+        1 => 1,
+        2 => 2,
+        _ => climb_stairs_r(cache, n - 1) + climb_stairs_r(cache, n - 2),
+    };
+
+    cache.insert(n, result);
+    result
+}
+
+fn climb_stairs(n: i32) -> i32 {
+    let mut cache = HashMap::<i32, i32>::new();
+    climb_stairs_r(&mut cache, n)
+}
+
+impl Solution {
+    #[must_use]
+    pub fn climb_stairs(n: i32) -> i32 {
+        climb_stairs(n)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::problem_70::climb_stairs;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(climb_stairs(2), 2);
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(climb_stairs(3), 3);
+    }
+}
---
+++ src/problem_71/mod.rs
@@ -0,0 +1,59 @@
+use crate::shared::Solution;
+
+fn simplify_path(path: &str) -> String {
+    let pieces = path.split('/').collect::<Vec<_>>();
+
+    let mut simplified = Vec::new();
+
+    for piece in pieces {
+        match piece {
+            ".." => {
+                simplified.pop();
+            },
+            "" | "." => {
+                // ignore
+            },
+            p => {
+                simplified.push(p);
+            },
+        }
+    }
+
+    if simplified.is_empty() {
+        return "/".to_string();
+    }
+
+    simplified.into_iter().fold(String::new(), |mut acc, curr| {
+        acc.push('/');
+        acc.push_str(curr);
+        acc
+    })
+}
+
+impl Solution {
+    #[must_use]
+    #[allow(clippy::needless_pass_by_value)]
+    pub fn simplify_path(path: String) -> String {
+        simplify_path(&path)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::problem_71::simplify_path;
+
+    #[test]
+    fn test_1() {
+        assert_eq!(simplify_path("/home/"), "/home");
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(simplify_path("/../"), "/");
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(simplify_path("/home//foo/"), "/home/foo");
+    }
+}
