102d09154838eb52c4515bea03fed22a46e7f3c4
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-03-11 18:39:33 -0800

    feat: problem 81 - 83

---
+++ src/problem_0081/mod.rs
@@ -0,0 +1,76 @@
+use std::cmp::Ordering;
+
+use crate::shared::Solution;
+
+fn search(nums: &[i32], target: i32) -> bool {
+    let mut offset = 0;
+    let mut end = nums.len();
+
+    while offset < end {
+        let middle = (end - offset) / 2 + offset;
+
+        if nums[middle] == target {
+            return true;
+        }
+
+        match nums[middle].cmp(&nums[end - 1]) {
+            Ordering::Greater => {
+                if target >= nums[offset] && target < nums[middle] {
+                    end -= 1;
+                } else {
+                    offset = middle + 1;
+                }
+            },
+            Ordering::Less => {
+                if target > nums[middle] && target <= nums[end - 1] {
+                    offset = middle + 1;
+                } else {
+                    end -= 1;
+                }
+            },
+            Ordering::Equal => {
+                while offset < nums.len() && nums[offset] == nums[middle] {
+                    offset += 1;
+                }
+                while end > 0 && nums[end - 1] == nums[middle] {
+                    end -= 1;
+                }
+            },
+        }
+    }
+    false
+}
+
+impl Solution {
+    #[must_use]
+    #[allow(clippy::needless_pass_by_value)]
+    pub fn search2(nums: Vec<i32>, target: i32) -> bool {
+        search(&nums, target)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::problem_0081::search;
+
+    #[test]
+    fn test_1() {
+        let input = vec![2, 5, 6, 0, 0, 1, 2];
+
+        assert!(search(&input, 0));
+    }
+
+    #[test]
+    fn test_2() {
+        let input = vec![2, 5, 6, 0, 0, 1, 2];
+
+        assert!(!search(&input, 3));
+    }
+
+    #[test]
+    fn test_3() {
+        let input = vec![1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1];
+
+        assert!(search(&input, 2));
+    }
+}
---
+++ src/problem_0082/mod.rs
@@ -0,0 +1,100 @@
+use crate::shared::{ListNode, Solution};
+
+fn delete_duplicates_r_mut(head: &mut Option<Box<ListNode>>) {
+    let h = match head {
+        Some(h) => h,
+        None => {
+            return;
+        },
+    };
+
+    let current_val = h.val;
+
+    let mut next = h.next.take();
+    let mut delete_current = false;
+    while let Some(c) = next {
+        let next_val = c.val;
+
+        if next_val == current_val {
+            delete_current = true;
+            next = c.next;
+        } else {
+            next = Some(c);
+            break;
+        }
+    }
+
+    delete_duplicates_r_mut(&mut next);
+    if delete_current {
+        *head = next;
+    } else {
+        h.next = next;
+    }
+}
+
+// fn delete_duplicates_r(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     match head {
+//         Some(mut h) => {
+//             let current_val = h.val;
+
+//             let mut next = h.next.take();
+//             let mut delete_current = false;
+//             while let Some(c) = next {
+//                 let next_val = c.val;
+
+//                 if next_val == current_val {
+//                     delete_current = true;
+//                     next = c.next;
+//                 } else {
+//                     next = Some(c);
+//                     break;
+//                 }
+//             }
+
+//             if delete_current {
+//                 delete_duplicates_r(next)
+//             } else {
+//                 h.next = delete_duplicates_r(next);
+//                 Some(h)
+//             }
+//         },
+//         None => None,
+//     }
+// }
+// fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+//     delete_duplicates_r(head)
+// }
+
+fn delete_duplicates(mut head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+    delete_duplicates_r_mut(&mut head);
+    head
+}
+impl Solution {
+    #[must_use]
+    pub fn delete_duplicates(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        delete_duplicates(head)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{problem_0082::delete_duplicates, shared::to_ll};
+
+    #[test]
+    fn test_1() {
+        assert_eq!(
+            delete_duplicates(to_ll(&[1, 2, 3, 3, 4, 4, 5])),
+            to_ll(&[1, 2, 5])
+        );
+    }
+
+    #[test]
+    fn test_2() {
+        assert_eq!(delete_duplicates(to_ll(&[1, 1, 1, 2, 3])), to_ll(&[2, 3]));
+    }
+
+    #[test]
+    fn test_3() {
+        assert_eq!(delete_duplicates(to_ll(&[])), to_ll(&[]));
+    }
+}
