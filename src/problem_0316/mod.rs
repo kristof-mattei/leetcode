46804335f295a310386813770c9f8cf78ef7a1e9
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-03-21 09:04:19 -0700

    chore: removed broken code

--- src/problem_0316/mod.rs
+++ src/problem_0316/mod.rs
@@ -1,60 +1,35 @@
 use crate::shared::Solution;
-// fn remove_duplicate_letters(s: &str) -> String {
-//     let s_bytes = s.as_bytes();
 
-//     let mut char_count = [0; 26];
-//     let mut visited = [false; 26];
-
-//     let mut result = Vec::new();
-
-//     for i in 0..s.len() {
-//         char_count[(s_bytes[i] - b'a') as usize] += 1;
-//     }
-
-//     for i in 0..s.len() {
-//         char_count[(s_bytes[i] - b'a') as usize] -= 1;
-
-//         if !visited[(s_bytes[i] - b'a') as usize] {
-//             while result.last().map_or(false, |&v| v > s_bytes[i])
-//                 && char_count[result.last().map(|v| v - b'a').unwrap() as usize] > 0
-//             {
-//                 // marking letter visited
-//                 visited[result.last().map(|v| v - b'a').unwrap() as usize] = false;
-//                 result.pop();
-//             }
-
-//             // Add s[i] in res and
-//             // mark it visited
-//             result.push(s_bytes[i]);
-//             visited[(s_bytes[i] - b'a') as usize] = true;
-//         }
-//     }
-//     // return resultant string
-//     unsafe { String::from_utf8_unchecked(result) }
-// }
 fn remove_duplicate_letters(s: &str) -> String {
     let s_bytes = s.as_bytes();
 
-    let mut haves = [false; 26];
+    let mut char_count = [0; 26];
+    let mut visited = [false; 26];
 
-    s_bytes
-        .iter()
-        .for_each(|b| haves[(b - b'a') as usize] = true);
-    let mut result = Vec::<u8>::with_capacity(haves.iter().filter(|v| **v).count());
+    let mut result = Vec::new();
 
-    let mut r_index = s_bytes.len() - 1;
-
-    while haves.iter().any(|v| *v) {
-        let b = s_bytes[r_index];
-
-        if haves[(b - b'a') as usize] {
-            result.push(b);
-            haves[(b - b'a') as usize] = false;
-        }
-        r_index -= 1;
+    for i in 0..s.len() {
+        char_count[(s_bytes[i] - b'a') as usize] += 1;
     }
 
-    result.reverse();
+    for i in 0..s.len() {
+        char_count[(s_bytes[i] - b'a') as usize] -= 1;
+
+        if !visited[(s_bytes[i] - b'a') as usize] {
+            while result.last().map_or(false, |&v| {
+                v > s_bytes[i] && char_count[(v - b'a') as usize] > 0
+            }) {
+                // marking letter visited
+                visited[result.last().map(|v| v - b'a').unwrap() as usize] = false;
+                result.pop();
+            }
+
+            // Add s[i] in res and
+            // mark it visited
+            result.push(s_bytes[i]);
+            visited[(s_bytes[i] - b'a') as usize] = true;
+        }
+    }
     // return resultant string
     unsafe { String::from_utf8_unchecked(result) }
 }
