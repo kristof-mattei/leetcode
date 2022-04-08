0c84d10108dab30c6039fc91efef7b2031221f1a
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-04-08 16:40:36 -0400

    feat: problem 0132

--- src/problem_0132/mod.rs
+++ src/problem_0132/mod.rs
@@ -25,62 +25,30 @@ fn is_palindrome(chars: &[char]) -> bool {
     }
 }
 
-fn memoize<'a>(cache: &mut HashMap<&'a [char], usize>, chars: &'a [char]) -> usize {
-    if cache.contains_key(chars) {
-        cache[chars]
-    } else {
-        let r = split_up(cache, chars);
-
-        cache.insert(chars, r);
-
-        r
-    }
-}
-
 fn split_up<'a>(cache: &mut HashMap<&'a [char], usize>, chars: &'a [char]) -> usize {
-    let mut results = vec![];
-
     if is_palindrome(chars) {
-        results.push(vec![chars]);
+        return 0;
     }
 
-    for i in 1..chars.len() {
+    for i in (1..chars.len()).rev() {
         let (l, r) = chars.split_at(i);
 
         if !is_palindrome(l) {
             continue;
         }
 
-        // recursively split up right
-        for mut right_side_split in memoize(cache, r) {
-            let mut temp = vec![l];
-
-            temp.append(&mut right_side_split);
-            results.push(temp);
-        }
+        return 1 + split_up(cache, r);
     }
 
-    results
+    unreachable!()
 }
 
 fn min_cut(s: &str) -> i32 {
     let chars = s.chars().collect::<Vec<_>>();
 
     let r = split_up(&mut HashMap::new(), &chars);
-    let mut result = vec![];
 
-    for rr in r {
-        let mut temp = vec![];
-        for rrr in rr {
-            let s = rrr.iter().collect::<String>();
-            temp.push(s);
-        }
-
-        result.push(temp);
-    }
-    let min = result.into_iter().map(|r| r.len()).min().unwrap();
-
-    min as i32 - 1
+    r as i32
 }
 
 impl Solution {
