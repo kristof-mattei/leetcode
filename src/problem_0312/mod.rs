f21348cf25472254fb75a769a56bb1a37e4417a7
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-05-19 10:12:43 -0700

    feat: problem 312

--- src/problem_0312/mod.rs
+++ src/problem_0312/mod.rs
@@ -1,5 +1,26 @@
 fn max_coins(nums: &[i32]) -> i32 {
-    0
+    let mut dp = vec![vec![0; nums.len()]; nums.len()];
+
+    for g in 0..nums.len() {
+        for (i, j) in (g..nums.len()).enumerate() {
+            let mut max = i32::MIN;
+            for k in i..=j {
+                let left = if k == i { 0 } else { dp[i][k - 1] };
+                let right = if k == j { 0 } else { dp[k + 1][j] };
+
+                let left_after_pop = if i == 0 { 1 } else { nums[i - 1] };
+                let right_after_pop = if j == nums.len() - 1 { 1 } else { nums[j + 1] };
+
+                let cost = left_after_pop * nums[k] * right_after_pop;
+
+                max = i32::max(max, left + right + cost);
+            }
+
+            dp[i][j] = max;
+        }
+    }
+
+    dp[0][nums.len() - 1]
 }
 
 impl Solution {
