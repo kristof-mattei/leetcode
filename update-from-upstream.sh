d8e3d163f9360db00b02e250dd52b329d107f907
Kristof Mattei <864376+Kristof-Mattei@users.noreply.github.com>
2022-09-29 10:08:37 -0700

    fix: updated script for easier integrations

--- update-from-upstream.sh
+++ update-from-upstream.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+git fetch origin
 git fetch upstream
 
-git checkout -b update-from-upstream
+git branch -D update-from-upstream
+git branch update-from-upstream origin/main
 git checkout update-from-upstream
 
 git merge upstream/main
