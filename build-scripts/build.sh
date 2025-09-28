f7b0a1189c2e2b834199e2fc876b95aa63c8a246
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-09-27 22:08:33 -0700

    chore(fmt): fmt

--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -5,15 +5,14 @@ c_compiler="gcc"
 cpp_compiler="g++"
 target_cpu=""
 
-
 case $TARGET in
     x86_64-unknown-linux-musl)
         c_compiler="x86_64-linux-musl-gcc"
         cpp_compiler="x86_64-linux-musl-g++"
 
-            if ! [[ -z "${TARGETVARIANT}" ]]; then
-                target_cpu="-Ctarget-cpu=x86-64-${TARGETVARIANT}"
-            fi
+        if ! [[ -z "${TARGETVARIANT}" ]]; then
+            target_cpu="-Ctarget-cpu=x86-64-${TARGETVARIANT}"
+        fi
 
         ;;
     aarch64-unknown-linux-musl)
