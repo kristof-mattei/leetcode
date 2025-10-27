20ef31e5fb4015f96f1df19c7107c10ed2afcf69
Kristof Mattei <864376+kristof-mattei@users.noreply.github.com>
2025-10-27 13:27:09 -0700

    feat: mimalloc

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,5 @@
-// use std::path::PathBuf;
-
-// use env_logger::Env;
-// use futures::{stream::FuturesUnordered, StreamExt};
-// use log::info;
-
-// use structopt::StructOpt;
-
-// #[derive(Debug, StructOpt)]
-// #[structopt(
-//     // name, // from Cargo.toml,
-//     about, // needed otherwise it doesn't show description from Cargo.toml,
-//     author // needed otherwise it doesn't show author from Cargo.toml
-// )]
-// struct Opt {
-//     #[structopt(
-//         // verbatim_doc_comment,
-//         help = "Some help",
-//         parse(from_os_str)
-//     )]
-//     some_value: PathBuf,
-// }
+#[global_allocator]
+static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
 fn foo() -> &'static str {
     "Foo"
